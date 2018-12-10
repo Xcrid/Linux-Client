506bf8f7c83d5207b5193f3a35ea0faa4bee2d0e
Xcrid <36483768+Xcrid@users.noreply.github.com>
2018-12-10 17:22:00 +0100

    Update thinger.h

--- src/thinger/core/thinger.h
+++ src/thinger/core/thinger.h
@@ -27,6 +27,7 @@
 #include "pson.h"
 #include "json.hpp"
 #include "json_encoder.hpp"
+#include "json_decoder.hpp"
 #include "thinger_map.hpp"
 #include "thinger_resource.hpp"
 #include "thinger_message.hpp"
